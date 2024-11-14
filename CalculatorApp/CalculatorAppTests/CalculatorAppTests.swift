ec49fb76c7797f3d687c15c31444d3dcc9503803
minseok <pms950106@gmail.com>
2024-11-14 19:31:21 +0900

    first commit

---
+++ CalculatorApp/CalculatorAppTests/CalculatorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppTests.swift
+//  CalculatorAppTests
+//
+//  Created by t2023-m0072 on 11/14/24.
+//
+
+import Testing
+@testable import CalculatorApp
+
+struct CalculatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
