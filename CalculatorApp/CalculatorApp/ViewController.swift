26d80e318903f02af5c3534fd5c38afdedd9d7b7
minseok <pms950106@gmail.com>
2024-11-14 20:19:11 +0900

    [FEAT] Level 2 구현

--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -11,10 +11,15 @@ import SnapKit
 class ViewController: UIViewController {
 
     let label = UILabel()
+    let buttonSeven = UIButton()
+    let buttonEight = UIButton()
+    let buttonNine = UIButton()
+    let buttonPlus = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        stackViewUI()
     }
     
     private func configureUI() {
@@ -30,6 +35,37 @@ class ViewController: UIViewController {
             $0.height.equalTo(100)
             $0.top.equalToSuperview().offset(200)
             $0.trailing.equalToSuperview().offset(-30)
+        }
+    }
+    
+    private func stackViewUI() {
+        buttonSeven.setTitle("7", for: .normal)
+        buttonEight.setTitle("8", for: .normal)
+        buttonNine.setTitle("9", for: .normal)
+        buttonPlus.setTitle("+", for: .normal)
+
+        [buttonSeven, buttonEight, buttonNine, buttonPlus]
+            .forEach {
+                $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+                $0.setTitleColor(.white, for: .normal)
+                $0.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+                $0.layer.cornerRadius = 40
+            }
+        
+        let horizontalStackView = UIStackView(arrangedSubviews: [buttonSeven, buttonEight, buttonNine, buttonPlus])
+        horizontalStackView.axis = .horizontal          // horizontal 스택뷰로 설정
+        horizontalStackView.backgroundColor = .black
+        horizontalStackView.spacing = 10
+        horizontalStackView.distribution = .fillEqually // 모든 요소가 동일한 크기로 분배됨
+        
+        view.addSubview(horizontalStackView)
+        
+        horizontalStackView.snp.makeConstraints {
+            $0.height.equalTo(80)
+            $0.top.equalTo(label.snp.bottom).offset(20) // label과 간격 20으로 설정
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.leading.equalToSuperview().offset(20)
+            
         }
         
     }
