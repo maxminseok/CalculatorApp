e19a5409f8c31fcd571dc4a1d7acbf71eebf9e34
minseok <pms950106@gmail.com>
2024-11-20 22:04:13 +0900

    [COMMENT] 너무 디테일한 주석 삭제

--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -13,12 +13,11 @@ class Calculatation {
     /// - Parameter expression: 연산을 진행할 문자열 ex."1+2+3"
     /// - Returns: Optional Int 타입의 연산 결과 값
     static func calculate(expression: String) -> Int? {
-        let expression = NSExpression(format: expression)   // 입력받은 문자열로 NSExpression 클래스의 인스턴스
+        let expression = NSExpression(format: expression)
+        
         if let result = expression.expressionValue(with: nil, context: nil) as? Int {
-            // 결과값 Int로 타입캐스팅, 성공시 반환
             return result
         } else {
-            // 결과값이 Int로 변환 불가능 할 경우 nil 반환
             return nil
         }
     }
@@ -38,11 +37,9 @@ class LabelManager {
     /// label.text를 변경하기 위해 호출되는 메서드
     /// - Parameter input: UIButton의 title
     func changeLabelText(_ input: String) {
-        // 타이틀이 숫자일 때 handleNumberInput 메서드 호출
         if let number = Int(input) {
             handleNumberInput(input)
         }
-        // 타이틀이 기호일 때 inputOperation 메서드 호출
         else {
             handleOperationInput(input)
         }
@@ -50,7 +47,7 @@ class LabelManager {
     
     /// label.text의 첫 글자가 0인지 판단 후 label.text 값 변경
     /// - Parameter input: UIButton의 title
-    func handleNumberInput(_ input: String) { // 매개변수로 버튼의 타이틀이 넘어옴
+    func handleNumberInput(_ input: String) {
         let first = label.text?.first
         first == "0" ? label.text = input : label.text?.append(input)
     }
@@ -58,13 +55,11 @@ class LabelManager {
     /// 누른 버튼이 기호일 때 호출되는 메서드
     /// - Parameter input: UIButton의 title
     func handleOperationInput(_ input: String) {
-        // 기호가 "=" 일 때
         if input == "=" {
             if let validResult = Calculatation.calculate(expression: label.text ?? "0") {
                 label.text = String(validResult)
             }
         }
-        // 기호가 "AC" 또는 연산 기호일때
         else {
             input == "AC" ? label.text = "0" : updateLastChar(input)
         }
@@ -74,14 +69,13 @@ class LabelManager {
     /// - Parameter input: UIButton의 title
     func updateLastChar(_ input: String) {
         let last = label.text?.last
-        // label.text의 마지막 입력이 연산 기호일 때
+
         if last == "+" || last == "-" || last == "*" || last == "/" {
-            var text = label.text   // label.text의 값 text에 저장
-            text?.removeLast()  // 마지막 기호 제거
-            text?.append(input) // 새로 입력한 연산 기호 버튼의 타이틀을 마지막에 추가
-            label.text = text   // label.text의 값을 새로운 문자열로 교체
+            var text = label.text
+            text?.removeLast()
+            text?.append(input)
+            label.text = text
         }
-        // label.text의 마지막 문자가 연산기호가 아닐때
         else {
             label.text?.append(input)
         }
@@ -100,19 +94,20 @@ class ButtonManager {
     /// - Returns: UIButton
     func makeButtons(_ titleValues: String) -> UIButton {
         let button = UIButton()
-        button.setTitle(titleValues, for: .normal) // 모든 버튼의 타이틀을 입력받은 titleValues로 설정
-        button.setTitleColor(.white, for: .normal)  // 모든 버튼의 title 색을 하얀색으로 설정
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30) // 모든 버튼의 폰트를 bold, 크기 30으로 설정
-        button.layer.cornerRadius = 40 // 모든 버튼의 모서리를 40으로 설정
         
-        button.addTarget(self, action: #selector(clickButton), for: .touchDown) // 버튼이 눌렸을 때 clickButton 메서드 호출
+        button.setTitle(titleValues, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+        button.layer.cornerRadius = 40
+        
+        button.addTarget(self, action: #selector(clickButton), for: .touchDown)
         
         if let _ = Int(titleValues) {
-            button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0) // 숫자 버튼의 배경색 설정
+            button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
             return button
         }
         else {
-            button.backgroundColor = .orange // title이 숫자가 아닐 경우 배경을 오랜지색으로 설정
+            button.backgroundColor = .orange
             return button
         }
     }
@@ -120,8 +115,8 @@ class ButtonManager {
     /// 버튼이 눌렸을 때 동작 하는 메서드
     /// - Parameter sender: 클릭 이벤트를 발생시킨 UIButton 객체
     @objc func clickButton(_ sender: UIButton) {
-        if let title = sender.title(for: .normal) { // 타이틀 값은 옵셔널 타입이라 옵셔널 바인딩
-            labelManager.changeLabelText(title)  // 버튼의 타이틀을 매개변수로 전달
+        if let title = sender.title(for: .normal) {
+            labelManager.changeLabelText(title)
         }
     }
 }
@@ -130,15 +125,16 @@ class StackViewMaker {
     /// horizontal Stack View를 생성하는 메서드
     /// - Parameter views:horizontal Stack View에 들어갈 UIButton 배열
     /// - Returns: horizontal UIStackView
-    static func makeHorizontalStackView(_ views: [UIView]) -> UIStackView { // 버튼을 배열로 입력 받아서 스택뷰를 반환
+    static func makeHorizontalStackView(_ views: [UIView]) -> UIStackView {
         let horizontalStackView = UIStackView(arrangedSubviews: views)
+        
         horizontalStackView.axis = .horizontal
         horizontalStackView.backgroundColor = .black
         horizontalStackView.spacing = 10
         horizontalStackView.distribution = .fillEqually
         
         horizontalStackView.snp.makeConstraints {
-            $0.height.equalTo(80)   // 스택 뷰 높이 80으로 설정
+            $0.height.equalTo(80)
         }
         
         return horizontalStackView
@@ -146,9 +142,10 @@ class StackViewMaker {
     
     /// vertical Stack View를 생성하는 메서드
     /// - Parameter stackViewList: vertical Stack View에 들어갈 horizontal UIStackView 배열
-    static func makeVerticalStackView(_ stackViewList: [UIStackView]) -> UIStackView {    // 스택뷰를 배열로 받아서 세로로 쌓는 스택뷰
+    static func makeVerticalStackView(_ stackViewList: [UIStackView]) -> UIStackView {
         let verticalStackView = UIStackView(arrangedSubviews: stackViewList)
-        verticalStackView.axis = .vertical  // 세로 스택뷰로 설정
+        
+        verticalStackView.axis = .vertical
         verticalStackView.backgroundColor = .black
         verticalStackView.spacing = 10
         verticalStackView.distribution = .fillEqually
