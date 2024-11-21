7fc8a1f82b65ad08eb1d0b5133bdc701f90b4086
minseok <pms950106@gmail.com>
2024-11-21 15:14:47 +0900

    [REFACTOR] makeLabel, makeButton 클래스 따로 분리, click이벤트 처리 viewController로 이동

--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -24,20 +24,18 @@ class Calculatation {
 }
 
 class LabelManager {
+    var inputText: String
     var label: UILabel
     
     init(label: UILabel) {
-        self.label = label        
-        label.text = "0"
-        label.textColor = .white
-        label.textAlignment = .right
-        label.font = UIFont.boldSystemFont(ofSize: 60)
+        self.label = label
+        inputText = "0"
     }
-
+    
     /// label.text를 변경하기 위해 호출되는 메서드
     /// - Parameter input: UIButton의 title
     func changeLabelText(_ input: String) {
-        if let number = Int(input) {
+        if let _ = Int(input) {
             handleNumberInput(input)
         }
         else {
@@ -48,59 +46,87 @@ class LabelManager {
     /// label.text의 첫 글자가 0인지 판단 후 label.text 값 변경
     /// - Parameter input: UIButton의 title
     func handleNumberInput(_ input: String) {
-        let first = label.text?.first
-        first == "0" ? label.text = input : label.text?.append(input)
+        if inputText == "0" {
+            inputText = input
+        }
+        else {
+            inputText.append(input)
+        }
+        label.text = inputText
     }
     
     /// 누른 버튼이 기호일 때 호출되는 메서드
     /// - Parameter input: UIButton의 title
     func handleOperationInput(_ input: String) {
-        if input == "=" {
-            if let validResult = Calculatation.calculate(expression: label.text ?? "0") {
-                label.text = String(validResult)
-            }
-        }
-        else {
-            input == "AC" ? label.text = "0" : updateLastChar(input)
+        switch input {
+        case "=":
+            performCalculation()
+        case "AC":
+            resetLabel()
+        default:
+            updateLastChar(input)
         }
     }
     
     /// 연산 기호가 연속으로 눌리는 것을 막기 위한 메서드
     /// - Parameter input: UIButton의 title
     func updateLastChar(_ input: String) {
-        let last = label.text?.last
+        let last = inputText.last
+        
+        if isOperator(last) {
+            inputText.removeLast()
+        }
+        inputText.append(input)
+        label.text = inputText
+    }
+    
+    /// "=" 버튼이 눌렸을 때 동작하는 메서드
+    func performCalculation() {
+        if let result = Calculatation.calculate(expression: inputText) {
+            inputText = String(result)
+        } else {
+            inputText = "Error"
+        }
+        label.text = inputText
+    }
+    
+    /// "AC" 버튼이 눌렸을 때 동작하는 메서드
+    func resetLabel() {
+        inputText = "0"
+        label.text = inputText
+    }
+    
+    /// 연산 기호가 눌렸을 때 마지막 글자를 체크하는 메서드
+    /// - Parameter character: label의 마지막 글자
+    /// - Returns: 마지막 글자가 연산기호면 true, 아니면 false 반환
+    func isOperator(_ character: Character?) -> Bool {
+        return character == "+" || character == "-" || character == "*" || character == "/"
+    }
+}
 
-        if last == "+" || last == "-" || last == "*" || last == "/" {
-            var text = label.text
-            text?.removeLast()
-            text?.append(input)
-            label.text = text
-        }
-        else {
-            label.text?.append(input)
-        }
+class LabelMaker {
+    static func makeLabel() -> UILabel {
+        let label = UILabel()
+        label.text = "0"
+        label.textColor = .white
+        label.textAlignment = .right
+        label.font = UIFont.boldSystemFont(ofSize: 60)
+        
+        return label
     }
 }
 
 class ButtonManager {
-    let labelManager: LabelManager
-    
-    init(labelManager: LabelManager) {
-        self.labelManager = labelManager
-    }
-    
     /// UIButton을 생성하는 메서드
     /// - Parameter titleValues: UIButton의 title이 될 문자열
     /// - Returns: UIButton
-    func makeButtons(_ titleValues: String) -> UIButton {
+    static func makeButtons(_ titleValues: String) -> UIButton {
         let button = UIButton()
         
         button.setTitle(titleValues, for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
         button.layer.cornerRadius = 40
-        
-        button.addTarget(self, action: #selector(clickButton), for: .touchDown)
         
         if let _ = Int(titleValues) {
             button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
@@ -112,13 +138,6 @@ class ButtonManager {
         }
     }
     
-    /// 버튼이 눌렸을 때 동작 하는 메서드
-    /// - Parameter sender: 클릭 이벤트를 발생시킨 UIButton 객체
-    @objc func clickButton(_ sender: UIButton) {
-        if let title = sender.title(for: .normal) {
-            labelManager.changeLabelText(title)
-        }
-    }
 }
 
 class StackViewMaker {
@@ -132,7 +151,6 @@ class StackViewMaker {
         horizontalStackView.backgroundColor = .black
         horizontalStackView.spacing = 10
         horizontalStackView.distribution = .fillEqually
-        
         horizontalStackView.snp.makeConstraints {
             $0.height.equalTo(80)
         }
@@ -157,32 +175,37 @@ class StackViewMaker {
 class ViewController: UIViewController {
     
     var labelManager: LabelManager!
-    var buttonManager: ButtonManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setUI()
+    }
+    
+    private func setUI() {
         view.backgroundColor = .black
         
         // 라벨 생성
-        let label = UILabel()
-        labelManager = LabelManager(label: label)
-        buttonManager = ButtonManager(labelManager: labelManager)
-        view.addSubview(label) // 라벨을 뷰에 추가
-        
+        let label = LabelMaker.makeLabel()
+        view.addSubview(label)
         label.snp.makeConstraints {
             $0.height.equalTo(100)
             $0.top.equalToSuperview().offset(200)
             $0.trailing.equalToSuperview().offset(-30)
         }
+        labelManager = LabelManager(label: label)
         
         // 버튼의 타이틀 배열
         let buttonTitles = ["7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "AC", "0", "=", "/"]
         // 버튼 생성
-        let buttons = buttonTitles.map { buttonManager.makeButtons($0) }
+        let buttons = buttonTitles.map { title in
+            let button = ButtonManager.makeButtons(title)
+            button.addTarget(self, action: #selector(clickButton), for: .touchDown)
+            return button
+        }
         
         // 수평 스택 뷰 배열
         var horizontalStackViews: [UIStackView] = []
-        
         for index in stride(from: 0, to: buttons.count, by: 4) {
             let selectedButton = Array(buttons[index..<index+4])
             let horizontalStackView = StackViewMaker.makeHorizontalStackView(selectedButton)
@@ -191,13 +214,19 @@ class ViewController: UIViewController {
         
         // 수평 스택뷰 배열을 수직 스택뷰에 넣기
         let verticalStackView = StackViewMaker.makeVerticalStackView(horizontalStackViews)
-        
         view.addSubview(verticalStackView)
-        
         verticalStackView.snp.makeConstraints {
             $0.width.equalTo(350)
             $0.top.equalTo(label.snp.bottom).offset(60)
             $0.leading.trailing.equalToSuperview().inset(20)
+        }
+    }
+    
+    /// 버튼이 눌렸을 때 호출되는 메서드
+    /// - Parameter sender: 클릭 이벤트를 발생시킨 UIButton 객체
+    @objc func clickButton(_ sender: UIButton) {
+        if let title = sender.title(for: .normal) {
+            labelManager.changeLabelText(title)
         }
     }
 }
